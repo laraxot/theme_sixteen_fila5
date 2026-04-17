a60b60164a2317711927428db52d524c4fa203ba
marco76tv <marco.sottana@gmail.com>
2026-04-17 10:19:51 +0200

    .

--- resources/views/components/blocks/listing/pagination.blade.php
+++ resources/views/components/blocks/listing/pagination.blade.php
@@ -6,27 +6,38 @@
 ])
 
 <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <nav class="cmp-pagination" aria-label="Paginazione">
-                <ul class="pagination">
-                    <li class="page-item">
-                        <a class="page-link" href="{{ $pages[0]['url'] ?? '#' }}">{{ $previous_label }}</a>
-                    </li>
+    <div class="row my-4">
+        <nav class="pagination-wrapper justify-content-center" aria-label="Paginazione">
+            <ul class="pagination">
+                <li class="page-item {{ (int) $current_page === 1 ? 'disabled' : '' }}">
+                    <a class="page-link" href="{{ $pages[0]['url'] ?? '#' }}" {{ (int) $current_page === 1 ? 'tabindex="-1" aria-hidden="true"' : '' }}>
+                        <svg class="icon icon-primary">
+                            <use href="sprites.svg#it-chevron-left"></use>
+                        </svg>
+                        <span class="visually-hidden">{{ $previous_label }}</span>
+                    </a>
+                </li>
 
-                    @foreach($pages as $page)
-                        <li class="page-item {{ (int) ($page['number'] ?? 0) === (int) $current_page ? 'active' : '' }}">
-                            <a class="page-link" href="{{ $page['url'] ?? '#' }}">
-                                {{ $page['label'] ?? $page['number'] ?? '' }}
-                            </a>
-                        </li>
-                    @endforeach
-
-                    <li class="page-item">
-                        <a class="page-link" href="{{ $pages[count($pages) - 1]['url'] ?? '#' }}">{{ $next_label }}</a>
+                @foreach($pages as $page)
+                    <li class="page-item {{ (int) ($page['number'] ?? 0) === (int) $current_page ? 'active' : '' }}">
+                        <a class="page-link" href="{{ $page['url'] ?? '#' }}" {{ (int) ($page['number'] ?? 0) === (int) $current_page ? 'aria-current="page"' : '' }}>
+                            @if((int) ($page['number'] ?? 0) === 1)
+                                <span class="d-inline-block d-sm-none">Pagina </span>
+                            @endif
+                            {{ $page['label'] ?? $page['number'] ?? '' }}
+                        </a>
                     </li>
-                </ul>
-            </nav>
-        </div>
+                @endforeach
+
+                <li class="page-item {{ (int) $current_page === count($pages) ? 'disabled' : '' }}">
+                    <a class="page-link" href="{{ $pages[count($pages) - 1]['url'] ?? '#' }}" {{ (int) $current_page === count($pages) ? 'tabindex="-1" aria-hidden="true"' : '' }}>
+                        <span class="visually-hidden">{{ $next_label }}</span>
+                        <svg class="icon icon-primary">
+                            <use href="sprites.svg#it-chevron-right"></use>
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </div>
--- resources/views/components/blocks/listing/search-results.blade.php
+++ resources/views/components/blocks/listing/search-results.blade.php
@@ -5,68 +5,68 @@
     'items' => [],
 ])
 
-<section class="section">
+<div class="bg-grey-card py-5">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="cmp-heading pb-3 pb-lg-4">
-                    <h2>{{ $title }}</h2>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-12">
-                <form class="cmp-input-search mb-4" role="search">
-                    <div class="input-group">
-                        <input type="search" class="form-control" aria-label="Cerca nelle novità">
+        <h2 class="title-xxlarge mb-4">{{ $title }}</h2>
+        <div class="cmp-input-search">
+            <div class="form-group autocomplete-wrapper mb-0">
+                <div class="input-group">
+                    <label for="search-input" class="visually-hidden">{{ $title }}</label>
+                    <input type="search" class="form-control" placeholder="Cerca per parola chiave" id="search-input" name="search">
+                    <div class="input-group-append">
                         <button class="btn btn-primary" type="button">{{ $button_label }}</button>
                     </div>
-                </form>
-
+                    <span class="autocomplete-icon" aria-hidden="true">
+                        <svg class="icon icon-sm icon-primary">
+                            <use href="sprites.svg#it-search"></use>
+                        </svg>
+                    </span>
+                </div>
                 @if($summary !== '')
-                    <p class="mb-4">{{ $summary }}</p>
+                    <p id="search-summary" class="u-grey-light text-paragraph-card mt-2 mb-30 mt-lg-3 mb-lg-40">{{ $summary }}</p>
                 @endif
             </div>
         </div>
 
         <div class="row g-4">
             @foreach($items as $item)
-                <div class="col-12 col-lg-6">
-                    <article class="card card-teaser card-teaser-image-top no-after rounded shadow-sm h-100">
-                        @if(!empty($item['image']))
-                            <div class="card-image-wrapper with-read-more">
-                                <div class="card-body p-0">
-                                    <img class="img-fluid" src="{{ $item['image'] }}" alt="{{ $item['image_alt'] ?? 'descrizione immagine' }}">
-                                </div>
-                            </div>
-                        @endif
-
-                        <div class="card-body">
-                            @if(!empty($item['category']) || !empty($item['date']))
-                                <div class="category-top">
-                                    @if(!empty($item['category']))
-                                        <span class="badge bg-primary">{{ $item['category'] }}</span>
-                                    @endif
-                                    @if(!empty($item['date']))
-                                        <span class="data">{{ $item['date'] }}</span>
-                                    @endif
+                <div class="col-md-6 col-xl-4">
+                    <div class="card-wrapper border border-light rounded shadow-sm">
+                        <article class="card card-teaser card-teaser-image-top no-after rounded h-100">
+                            @if(!empty($item['image']))
+                                <div class="img-responsive-wrapper">
+                                    <div class="img-responsive img-responsive-panoramic">
+                                        <figure class="img-wrapper">
+                                            <img class="img-fluid" src="{{ $item['image'] }}" alt="{{ $item['image_alt'] ?? 'descrizione immagine' }}">
+                                        </figure>
+                                    </div>
                                 </div>
                             @endif
 
-                            <h3 class="card-title h5">
-                                <a href="{{ $item['url'] ?? '#' }}" class="text-decoration-none">
-                                    {{ $item['title'] ?? '' }}
+                            <div class="card-body">
+                                @if(!empty($item['category']) || !empty($item['date']))
+                                    <div class="category-top">
+                                        @if(!empty($item['category']))
+                                            <a class="category text-decoration-none" href="#">{{ $item['category'] }}</a>
+                                        @endif
+                                        @if(!empty($item['date']))
+                                            <span class="data">{{ $item['date'] }}</span>
+                                        @endif
+                                    </div>
+                                @endif
+
+                                <a href="{{ $item['url'] ?? '#' }}" class="text-decoration-none" data-element="news-category-link">
+                                    <h3 class="card-title">{{ $item['title'] ?? '' }}</h3>
                                 </a>
-                            </h3>
 
-                            @if(!empty($item['description']))
-                                <p class="card-text font-serif">{{ $item['description'] }}</p>
-                            @endif
-                        </div>
-                    </article>
+                                @if(!empty($item['description']))
+                                    <p class="card-text text-secondary">{{ $item['description'] }}</p>
+                                @endif
+                            </div>
+                        </article>
+                    </div>
                 </div>
             @endforeach
         </div>
     </div>
-</section>
+</div>
