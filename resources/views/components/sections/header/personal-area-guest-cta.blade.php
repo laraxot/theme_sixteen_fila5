7940c7673b973fe259a53bc809f7cd7ce6a20a97
marco76tv <marco.sottana@gmail.com>
2026-04-21 12:53:38 +0200

    .

--- resources/views/components/sections/header/personal-area-guest-cta.blade.php
+++ resources/views/components/sections/header/personal-area-guest-cta.blade.php
@@ -1,6 +1,6 @@
-<a class="btn btn-primary btn-icon btn-full" href="{{ route('login') }}" data-element="personal-area-login">
+<a class="btn btn-primary btn-icon btn-full" href="{{ route('login') }}" data-element="personal-area-login" style="background-color: #007A52; border-color: #007A52;">
     <span class="rounded-icon" aria-hidden="true">
-        <svg class="icon icon-primary">
+        <svg class="icon icon-white">
             <use href="/themes/Sixteen/design-comuni/assets/bootstrap-italia/dist/svg/sprites.svg#it-user"></use>
         </svg>
     </span>
