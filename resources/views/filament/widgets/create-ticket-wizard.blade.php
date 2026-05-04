05fae53b6974b6c60c294bde0fc6d450caeef80e
marco76tv <marco.sottana@gmail.com>
2026-05-04 16:28:33 +0200

    .

--- resources/views/filament/widgets/create-ticket-wizard.blade.php
+++ resources/views/filament/widgets/create-ticket-wizard.blade.php
@@ -1,11 +1,14 @@
 @php
-    $sprite = '/themes/Sixteen/design-comuni/assets/bootstrap-italia/dist/svg/sprites.svg';
     $contacts = is_array($blockData['contacts'] ?? null) ? $blockData['contacts'] : [];
+    
+    // Extract wizard from form - DYNAMIC from module (no hardcoded steps!)
+    $wizardComponent = $this->getForm()?->getComponent('wizard');
+    $steps = $wizardComponent?->getSteps() ?? [];
 @endphp
 <x-filament-widgets::widget>
 {{-- Wrapper Design Comuni: titolo + form Filament Wizard (schema in CreateTicketWizardWidget::getFormSchema) --}}
 <div class="ticket-wizard-root">
-    {{-- Nessun id="main-container" qui: l'id è su <main> nel layout; duplicarlo rompe i selettori CSS e l'HTML. --}}
+    {{-- Heading section --}}
     <div class="container wizard-dc-heading-shell">
         <div class="row justify-content-center">
             <div class="col-12 col-lg-10">
@@ -19,24 +22,34 @@
         </div>
     </div>
 
+    {{-- Design Comuni Stepper - Dynamic from Wizard component --}}
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-12 col-lg-10 pb-40 pb-lg-80">
-                
-                    <form wire:submit="submit">
-                        @if ($errors->has('data.submit') || $errors->has('submit'))
-                            <div class="alert alert-danger mb-4" role="alert">
-                                {{ $errors->first('data.submit') ?: $errors->first('submit') }}
-                            </div>
-                        @endif
-                        {{ $this->form }}
-                    </form>
-                    
-                
+            <div class="col-12 col-lg-10">
+                <x-pub_theme::wizard.stepper 
+                    :steps="$steps" 
+                    :current-step="$wizardComponent?->getCurrentStepIndex() ?? 0"
+                    :total-steps="count($steps)"
+                />
             </div>
         </div>
     </div>
 
+    {{-- Wizard Form --}}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10 pb-40 pb-lg-80">
+                <form wire:submit="submit">
+                    @if ($errors->has('data.submit') || $errors->has('submit'))
+                        <div class="alert alert-danger mb-4" role="alert">
+                            {{ $errors->first('data.submit') ?: $errors->first('submit') }}
+                        </div>
+                    @endif
+                    {{ $this->form }}
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 <x-filament-actions::modals />
 </x-filament-widgets::widget>
